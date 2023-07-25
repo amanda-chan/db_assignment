c6ae7801b9dddf939c1abdd40ef5df11dcb4a373
huiyinghuan <huanhuiying.hhy@gmail.com>
2023-07-26 03:43:45 +0800

    working on review view

--- food_app/review.py
+++ food_app/review.py
@@ -9,7 +9,7 @@ review_bp = Blueprint("review", __name__, url_prefix="/review")
 
 @review_bp.route("/make-a-review")
 def make():
-    # add login required ^
+    # add login required
     #maybe need to add like booking = current booking then can, but now just try try to create
     return render_template("profile/review/makeReview.html", user=current_user)
 
